8eda77b168e78c8ebb2d77f12aca38e79999e12b
3karll2 <mihajloonisuk81@gmail.com>
2025-05-12 23:49:36 +0300

    Add files via upload

---
+++ Miha.py
@@ -0,0 +1,20 @@
+class Miha:
+    def __init__(self, name=None, surname=None, birth_year=None):
+        self.name = name
+        self.surname = surname
+        self.birth_year = birth_year
+
+    def get_course(self):
+        if self.birth_year:
+            course = 2025 - (self.birth_year + 16) + 1
+            return max(course, 1)
+        return None
+
+    def get_full_name_list(self):
+        return [self.name, self.surname]
+
+
+student = Miha("Михайло", "Оніщук", 2008)
+
+print("Курс:", student.get_course())
+print("ПІБ:", student.get_full_name_list())
